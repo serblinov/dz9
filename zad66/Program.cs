85e2fca92f9cec4dca4fcc5776057dd035cc0ca1
sergblin <blinofftash86@mail.ru>
2022-12-19 18:53:49 +0300

    сумма натуральных чисел

--- zad66/Program.cs
+++ zad66/Program.cs
@@ -3,23 +3,22 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30*/
 
-int resultNumber(int M, int N)
+int sumOfNaturalNumbers(int M, int N)
 {
-    int sum=0;
-    if(M>0 && N>0)
+
+    if(M>N || N <0)
     {
-         sum = (M+N)*(N-M+1)/2;
+         return 0;
     }
-    else if(N<M)
+    else 
     {
-         sum=0;
+         return  N + sumOfNaturalNumbers(M,N-1);
     }
-     return sum;
 }
 Console.Write("Введите натуральное число M: ");
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите натуральное число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Сумма натуральных чисел равна: {resultNumber(M,N)}");
+Console.WriteLine($"Сумма натуральных чисел равна: {sumOfNaturalNumbers(M,N)}");
 
 
