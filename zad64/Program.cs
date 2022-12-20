7626b9133190d1f3b17c3b80aa43a3b7ccedfc95
sergblin <blinofftash86@mail.ru>
2022-12-20 09:23:37 +0300

    integers

---
+++ zad64/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+ N = 5 -> "5, 4, 3, 2, 1"
+ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+string integers (int m, int n)
+{
+    if(m<=n) 
+    {
+        return integers(m+1,n) + $"{m}"+ ",";
+    }
+    else 
+    {
+        return string.Empty;
+    }
+}
+int m = 1;
+Console.WriteLine($"Введите натуральное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{integers(m,n)}");
+
